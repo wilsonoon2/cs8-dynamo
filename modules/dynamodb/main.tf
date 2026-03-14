a0a6e773a6a490a082a80219ada341c28b2da203
wilsonoon2 <wilson.oontb@outlook.com>
2026-03-14 23:05:44 +0800

    next commit

---
+++ modules/dynamodb/main.tf
@@ -0,0 +1,15 @@
+resource "aws_dynamodb_table" "this" {
+  name         = var.table_name
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Name = var.table_name
+  }
+}
